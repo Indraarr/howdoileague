289c3da2b1802abbc8505aebd55e7a9090d604a0
Indraarr <indrabachtiar123@gmail.com>
2025-04-28 03:25:53 +0800

    asd

--- script.js
+++ script.js
@@ -7,29 +7,29 @@ textbox.addEventListener("input", check);
 
 
 const champions = [
-    "aatrox", "ahri", "akali", "akshan", "alistar", "amara", "amumu", "anivia", "annie", "aphelios",
-    "ashe", "aurelion sol", "azir", "bard", "belveth", "blitzcrank", "brand", "braum", "briar",
-    "caitlyn", "camille", "cassiopeia", "cho'gath", "corki", "darius", "diana", "dr. mundo", "draven",
+    "aatrox", "ahri", "akali", "akshan", "alistar", "ambessa", "amumu", "anivia", "annie", "aphelios",
+    "ashe", "aurelion sol", "aurora", "azir", "bard", "belveth", "blitzcrank", "brand", "braum", "briar",
+    "caitlyn", "camille", "cassiopeia", "chogath", "corki", "darius", "diana", "dr. mundo", "draven",
     "ekko", "elise", "evelynn", "ezreal", "fiddlesticks", "fiora", "fizz", "galio", "gangplank",
     "garen", "gnar", "gragas", "graves", "gwen", "hecarim", "heimerdinger", "hwei", "illaoi", "irelia",
     "ivern", "janna", "jarvan iv", "jax", "jayce", "jhin", "jinx", "ksante", "kai'sa", "kalista",
-    "karma", "karthus", "kassadin", "katarina", "kayle", "kayn", "kennen", "kha'zix", "kindred",
-    "kled", "kog'maw", "leblanc", "lee sin", "leona", "lillia", "lissandra", "lucian", "lulu",
+    "karma", "karthus", "kassadin", "katarina", "kayle", "kayn", "kennen", "khazix", "kindred",
+    "kled", "kogmaw", "leblanc", "leesin", "leona", "lillia", "lissandra", "lucian", "lulu",
     "lux", "malphite", "malzahar", "maokai", "milio", "miss fortune", "mordekaiser", "morgana",
-    "naafiri", "nami", "nasus", "nautilus", "neeko", "nidalee", "nilah", "nocturne", "nunu & willump",
+    "naafiri", "nami", "nasus", "nautilus", "neeko", "nidalee", "nilah", "nocturne", "nunu",
     "olaf", "orianna", "ornn", "pantheon", "poppy", "pyke", "qiyana", "quinn", "rakan", "rammus",
-    "reksai", "rell", "renata glasc", "renekton", "rengar", "riven", "rumble", "ryze", "samira",
+    "reksai", "rell", "renata", "renekton", "rengar", "riven", "rumble", "ryze", "samira",
     "sejuani", "senna", "seraphine", "sett", "shaco", "shen", "shyvana", "singed", "sion", "sivir",
-    "skarner", "sona", "soraka", "swain", "sylas", "syndra", "tahm kench", "taliyah", "talon",
-    "taric", "teemo", "thresh", "tristana", "trundle", "tryndamere", "twisted fate", "twitch",
+    "skarner", "sona", "soraka", "swain", "sylas", "syndra", "tahmkench", "taliyah", "talon",
+    "taric", "teemo", "thresh", "tristana", "trundle", "tryndamere", "twistedfate", "twitch",
     "udyr", "urgot", "varus", "vayne", "veigar", "velkoz", "vex", "vi", "viego", "viktor", "vladimir",
-    "voli", "warwick", "wukong", "xerath", "xayah", "xin zhao", "yasuo", "yone", "yorick", "yuumi",
+    "volibear", "warwick", "wukong", "xerath", "xayah", "xinzhao", "yasuo", "yone", "yorick", "yuumi",
     "zac", "zed", "zeri", "ziggs", "zilean", "zoe", "zyra"
   ];
 
 
 function check() {
-    let text = textbox.value.toLowerCase();
+    let text = textbox.value.toLowerCase().replaceAll("'", "").replaceAll(" ","");
 
     if (textbox.value == "") {
         disappear(lastSeen);
@@ -51,7 +51,7 @@ function check() {
 
 
 function appear(champion) {
-    champion.style.height = "1000px";
+    champion.style.height = "1500px";
     champion.style.transition = "height 0.5s ease 0.2s"
 }
 
